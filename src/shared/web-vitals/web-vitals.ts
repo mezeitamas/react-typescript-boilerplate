3e112bb70159daeaf53319d603010b94ecd074ff
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2024-11-20 09:33:35 +0000

    chore: update deps and node

--- src/shared/web-vitals/web-vitals.ts
+++ src/shared/web-vitals/web-vitals.ts
@@ -3,7 +3,7 @@ import type { CLSMetric, INPMetric, LCPMetric } from 'web-vitals';
 
 const addWebVitalsReporting = () => {
     const logToConsole = ({ name, id, delta, rating }: CLSMetric | INPMetric | LCPMetric) => {
-        console.log(`${name} matching ID ${id} changed by ${String(delta)} and the rating is ${String(rating)}`);
+        console.log(`${name} matching ID ${id} changed by ${String(delta)} and the rating is ${rating}`);
     };
 
     onCLS(logToConsole);
